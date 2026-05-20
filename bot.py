cbb0a9c5d687a02510b3e1ceb27faf95321b1328
lazarevaist-afk <lazarevaist@gmail.com>
2026-05-20 17:04:05 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,3 +14,17 @@ app.add_handler(CommandHandler("start", start))
 if __name__ == "__main__":
     print("BOT STARTED")
     app.run_polling()
+import os
+
+port = int(os.environ.get("PORT", 10000))
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is running")
+
+server = HTTPServer(("0.0.0.0", port), Handler)
+server.serve_forever()

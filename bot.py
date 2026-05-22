89bc5f1dfa8ad1bded5a2adf9a0f36997aada38d
lazarevaist-afk <lazarevaist@gmail.com>
2026-05-22 16:33:20 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
+import threading
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
+# ================= TOKEN =================
 TOKEN = os.getenv("TOKEN")
 
 OWNER_ID = 8409916382
@@ -11,6 +14,7 @@ def allowed(user_id):
     return user_id == OWNER_ID
 
 
+# ================= TELEGRAM HANDLERS =================
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     if not allowed(update.effective_user.id):
@@ -68,6 +72,27 @@ app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("search", search))
 
-print("BOT STARTED")
 
-app.run_polling()
+# ================= FAKE SERVER (Render fix) =================
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+
+    class Handler(BaseHTTPRequestHandler):
+        def do_GET(self):
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"Bot is running")
+
+    server = HTTPServer(("0.0.0.0", port), Handler)
+    server.serve_forever()
+
+
+# ================= START =================
+if __name__ == "__main__":
+    print("BOT STARTED", flush=True)
+
+    # запускаем порт для Render
+    threading.Thread(target=run_web, daemon=True).start()
+
+    # запускаем Telegram bot
+    app.run_polling()
